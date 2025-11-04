98dfe507849b67c4fa0e44a79a6ac2690011d0cc
Cesar Sustaita <c.sustaita@outlook.com>
2025-11-04 16:27:50 -0600

    feat: Adds test/setup.js. Configures the JSDOM environment and fake timers.

---
+++ src/test/setup.js
@@ -0,0 +1,105 @@
+import { UIButton } from '@components/ui-button.js'; 
+import { UIconButton } from '@components/ui-icon-button.js'; 
+import { UINotification } from '@components/ui-notification.js'; 
+import { UISwitch } from '@components/ui-switch.js'; 
+import { UICheckbox } from '@components/ui-checkbox.js'; 
+import { UIRadioButton } from '@components/ui-radio-button.js'; 
+import { UITag } from '@components/ui-tag.js'; 
+import { UILabel } from '@components/ui-label.js'; 
+import { UInput } from '@components/ui-input.js'; 
+import { UICard } from '@components/ui-card.js'; 
+import { UIList } from '@components/ui-list.js'; 
+//import { UIDialog } from '@components/ui-dialog.js'; 
+import { UITooltip } from '@components/ui-tooltip.js'; 
+//import { UITab } from '@components/ui-tab.js'; 
+import { UIPagination } from '@components/ui-pagination.js'; 
+import { UINavigationBtn } from '@components/ui-navigation-btn.js'; 
+import { UIAccordion } from '@components/ui-accordion.js'; 
+import { UINotification } from '@components/ui-notification.js'; 
+
+
+
+import 'lit';
+
+
+
+
+// Importacion global para las pruebas
+if (!customElements.get('ui-button')) {
+  customElements.define('ui-button', UIButton);
+}
+
+if (!customElements.get('ui-icon-button')) {
+  customElements.define('ui-icon-button', UIconButton);
+}
+
+if (!customElements.get('ui-switch')) {
+  customElements.define('ui-switch', UISwitch);
+}
+
+if (!customElements.get('ui-checkbox')) {
+  customElements.define('ui-checkbox', UICheckbox);
+}
+
+if (!customElements.get('ui-radio-button')) {
+  customElements.define('ui-radio-button', UIRadioButton);
+}
+
+if (!customElements.get('ui-tag')) {
+  customElements.define('ui-tag', UITag);
+}
+
+if (!customElements.get('ui-label')) {
+  customElements.define('ui-label', UILabel);
+}
+
+if (!customElements.get('ui-input')) {
+  customElements.define('ui-input', UInput);
+}
+
+if (!customElements.get('ui-card')) {
+  customElements.define('ui-card', UICard);
+}
+
+if (!customElements.get('ui-list')) {
+  customElements.define('ui-list', UIList);
+}
+
+/*if (!customElements.get('ui-dialog')) {
+  customElements.define('ui-dialog', UIDialog);
+}*/
+
+if (!customElements.get('ui-tooltip')) {
+  customElements.define('ui-tooltip', UITooltip);
+}
+
+/*if (!customElements.get('ui-tab')) {
+  customElements.define('ui-tab', UITab);
+}*/
+
+if (!customElements.get('ui-pagination')) {
+  customElements.define('ui-pagination', UIPagination);
+}
+
+if (!customElements.get('ui-navigation-btn')) {
+  customElements.define('ui-navigation-btn', UINavigationBtn);
+}
+
+if (!customElements.get('ui-accordion')) {
+  customElements.define('ui-accordion', UIAccordion);
+}
+
+if (!customElements.get('ui-notification')) {
+  customElements.define('ui-notification', UINotification);
+}
+
+
+if (!customElements.get('ui-notification')) {
+  customElements.define('ui-notification', UINotification);
+}
+
+
+
+
+
+
